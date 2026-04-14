1976f4fb4af8bf05da8add0d8957f673565391bc
Claudio Perroni <c.perroni@ifortech.com>
2026-04-14 15:30:39 +0200

    inizio

---
+++ resources/views/components/layouts/app.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" data-theme="sodexo">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name', 'Laravel') }}</title>
+
+        <!-- Styles / Scripts -->
+        
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        
+    </head>
+
+    <body class="antialiased">
+        {{ $slot }}
+    </body>
+</html>
