ba0c855496ea228f852fa0332df46f668039711c
Claudio Perroni <c.perroni@ifortech.com>
2026-04-15 11:50:12 +0200

    sidenav

--- resources/views/components/layouts/admin.blade.php
+++ resources/views/components/layouts/admin.blade.php
@@ -17,9 +17,15 @@
         <div class="drawer-side">
             <label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"></label>
             <ul class="menu bg-base-200 min-h-full w-80 p-4">
-                <!-- Sidebar content here -->
-                <li><a>Sidebar Item 1</a></li>
-                <li><a>Sidebar Item 2</a></li>
+                <li>
+                    <a
+                        href="{{ route('admin.courses.index') }}"
+                        @class(['menu-active' => request()->routeIs('admin.courses.*')])
+                    >
+                        {{ __('Corsi') }}
+                    </a>
+                </li>
+
             </ul>
         </div>
     </div>
