d5d7a2d3f7cc2842f121557b99a58f93a7e729ce
erasmosalsanoifortech <e.salsano@ifortech.com>
2026-04-17 15:40:58 +0200

    Fix permessi pagine categorie di lavoro e simili

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,13 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->web(append: [
             EnsureUserOnboarded::class,
         ]);
+
+        // Registra middleware alias di Spatie Permission
+        $middleware->alias([
+            'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+            'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+            'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
