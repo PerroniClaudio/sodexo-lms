b809552cba2b8589833c9a1fd5fe3a6c9ffdf245
Claudio Perroni <c.perroni@ifortech.com>
2026-04-29 16:17:31 +0200

    cartella svg

--- tests/Feature/Auth/LoginViewTest.php
+++ tests/Feature/Auth/LoginViewTest.php
@@ -1,6 +1,6 @@
 <?php
 
-test('login view uses daisyui fieldset markup for credentials', function () {
+test('login view renders lucide icons and daisyui fieldset markup for credentials', function () {
     $response = $this->get('/login');
 
     $response
@@ -11,6 +11,8 @@ test('login view uses daisyui fieldset markup for credentials', function () {
         ->assertSee('class="input w-full', false)
         ->assertDontSee('Usa l&#039;email con cui accedi all&#039;area riservata', false)
         ->assertDontSee('La password del tuo account', false)
+        ->assertSee('class="swap-off h-4 w-4"', false)
+        ->assertSee('class="swap-on h-4 w-4"', false)
         ->assertSee('data-password-input', false)
         ->assertSee('data-password-toggle', false)
         ->assertSee('class="swap swap-rotate cursor-pointer text-base-content/60"', false)
