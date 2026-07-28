c965589222f34c9ba5eb11802fbfee596aad8d7f
Claudio Perroni <c.perroni@ifortech.com>
2026-07-28 12:08:24 +0200

    footer

--- resources/views/components/homepage/footer.blade.php
+++ resources/views/components/homepage/footer.blade.php
@@ -17,9 +17,9 @@
             </span>
         </div>
 
-        <div class="flex gap-4 text-sm font-semibold">
+        {{-- <div class="flex gap-4 text-sm font-semibold">
             <a href="{{ url('/cookie-policy') }}" class="hover:text-primary">Cookie</a>
             <a href="{{ url('/privacy-policy') }}" class="hover:text-primary">Privacy policy</a>
-        </div>
+        </div> --}}
     </div>
 </footer>
